ccfbeebc6f241b1842a9da8aad1307caf1854c8d
Seung Woo (Paul) Ji <wincow.ji@gmail.com>
2020-11-24 22:54:08 -0500

    Updated Workshop 9 Part 1

---
+++ OOP-Workshops/WS09/part 1/SecureData.cpp
@@ -0,0 +1,149 @@
+/* ------------------------------------------------------
+Workshop 9: Multi-Threading
+Module: SecureData
+Filename: SecureData.cpp
+Version: 1.0
+Author: Seung Woo Ji
+Student Number: 116376195
+Email: swji1@myseneca.ca
+Revision History
+-----------------------------------------------------------
+Date		Reason
+2020/11/20	Completed workshop 9 part 1 
+-----------------------------------------------------------
+I have done all the coding by myself and only copied the code
+that my professor provided to complete my workshops and assignments.
+-----------------------------------------------------------*/
+// Workshop 9 - Multi-Threading
+// SecureData.cpp
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <thread>
+#include <functional>
+#include "SecureData.h"
+
+using namespace std;
+
+namespace w9 {
+
+	void converter(char* t, char key, int n, const Cryptor& c) {
+		for (int i = 0; i < n; i++)
+			t[i] = c(t[i], key);
+	}
+
+	SecureData::SecureData(const char* file, char key, ostream* pOfs)
+	{
+		ofs = pOfs;
+
+		// open text file
+		fstream input(file, std::ios::in);
+		if (!input)
+			throw string("\n***Failed to open file ") +
+			string(file) + string(" ***\n");
+
+		// copy from file into memory
+		input.seekg(0, std::ios::end);
+		nbytes = (int)input.tellg() + 1;
+
+		text = new char[nbytes];
+
+		input.seekg(ios::beg);
+		int i = 0;
+		input >> noskipws;
+		while (input.good())
+			input >> text[i++];
+		text[nbytes - 1] = '\0';
+		*ofs << "\n" << nbytes - 1 << " bytes copied from file "
+			<< file << " into memory (null byte added)\n";
+		encoded = false;
+
+		// encode using key
+		code(key);
+		*ofs << "Data encrypted in memory\n";
+	}
+
+	SecureData::~SecureData() {
+		delete[] text;
+	}
+
+	void SecureData::display(std::ostream& os) const {
+		if (text && !encoded)
+			os << text << std::endl;
+		else if (encoded)
+			throw std::string("\n***Data is encoded***\n");
+		else
+			throw std::string("\n***No data stored***\n");
+	}
+
+	void SecureData::code(char key)
+	{
+		converter(text, key, nbytes, Cryptor());
+
+		encoded = !encoded;
+	}
+
+	void SecureData::backup(const char* file) {
+		if (!text)
+			throw std::string("\n***No data stored***\n");
+		else if (!encoded)
+			throw std::string("\n***Data is not encoded***\n");
+		else
+		{
+			// TODO: open a binary file for writing
+			std::fstream binaryFile(file, std::ios::out | std::ios::binary | std::ios::trunc);
+
+			// TODO: write data into the binary file
+			//         and close the file	
+			binaryFile.write(this->text, this->nbytes);
+
+			binaryFile.close();
+		}
+	}
+
+	void SecureData::restore(const char* file, char key) {
+		// TODO: open binary file for reading
+		std::fstream binaryFile(file, std::ios::in | std::ios::binary);
+		
+		if (!binaryFile)
+		{
+			throw string("\n***Failed to open file ") +
+				string(file) + string(" ***\n");
+		}
+
+		// deallocate the pre-owned memory if existed
+		if (text)
+		{
+			delete[] this->text;
+			this->nbytes = 0;
+		}
+		
+		// TODO: - allocate memory here for the file content
+		binaryFile.seekg(0, std::ios::end);
+		this->nbytes = (int)binaryFile.tellg();
+	
+		this->text = new char[this->nbytes];
+
+		// TODO: - read the content of the binary file
+		binaryFile.seekg(std::ios::beg);
+		binaryFile.read(this->text, this->nbytes);
+
+		binaryFile.close();
+		
+		*ofs << "\n" << nbytes << " bytes copied from binary file "
+			<< file << " into memory.\n";
+
+		encoded = true;
+
+		// decode using key
+		code(key);
+
+		*ofs << "Data decrypted in memory\n\n";
+	}
+
+	std::ostream& operator<<(std::ostream& os, const SecureData& sd) {
+		sd.display(os);
+		return os;
+	}
+}
---
+++ OOP-Workshops/WS09/part 1/w9.cpp
@@ -0,0 +1,55 @@
+/* ------------------------------------------------------
+Workshop 9: Multi-Threading
+Module: w9
+Filename: w9.cpp
+Version: 1.0
+Author: Seung Woo Ji
+Student Number: 116376195
+Email: swji1@myseneca.ca
+Revision History
+-----------------------------------------------------------
+Date		Reason
+2020/11/20	Completed workshop 9 part 1
+-----------------------------------------------------------
+I have done all the coding by myself and only copied the code
+that my professor provided to complete my workshops and assignments.
+-----------------------------------------------------------*/
+// Workshop 9 - Multi-Threading
+// w9.cpp
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include "SecureData.h"
+#include "SecureData.h"
+
+using namespace std;
+
+int main(int argc, char** argv)
+{
+	cout << "Command Line: " << argv[0];
+	for (int i = 1; i < argc; i++)
+		cout << " " << argv[i];
+	cout << endl << endl;
+
+	if (argc != 4)
+	{
+		cerr << endl << "***Incorrect number of arguments***" << endl;
+		return 1;
+	}
+
+    char key = argv[3][0];
+
+    try {
+        w9::SecureData sd(argv[1], key, &cout);
+		sd.backup(argv[2]);
+
+		sd.restore(argv[2], key);
+		cout << sd << std::endl;
+
+    } catch (const std::string& msg) {
+        cout << msg << std::endl;
+    }
+
+	return 0;
+}
